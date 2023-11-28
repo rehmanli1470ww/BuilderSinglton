eb1d456015111e749adb317934c5a67bb84df819
rehmanli1470ww <144922823+rehmanli1470ww@users.noreply.github.com>
2023-11-28 22:01:23 +0400

    Add project files.

---
+++ BuilderSinglton/Program.cs
@@ -0,0 +1,99 @@
+﻿
+class Pro
+{
+    interface Builder
+    {
+        public void reset();
+        public void setSeats(int number);
+        public void setEnigne(SportsEnigne sportsEnigne);
+        public void setTripComputer();
+        public void setGPS();
+    }
+    public class SportsEnigne { }
+
+    class Car { }
+    class Manual { }
+
+    class CarBuilder : Builder
+    {
+        private Car? car;
+        public void reset() { }
+        
+
+        public void setEnigne(SportsEnigne sportsEnigne) { }
+       
+
+        public void setGPS() { }
+        
+
+        public void setSeats(int number) { }
+       
+
+        public void setTripComputer() { }
+       
+
+        public Car getResault()
+        {
+            return car!;
+        }
+    }
+
+    class CarManualBuilder : Builder
+    {
+        private Manual? manual;
+
+        public void reset()
+        {
+            this.manual = new Manual();
+        }
+
+        public void setEnigne(SportsEnigne sportsEnigne)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setGPS()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setSeats(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setTripComputer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Manual getManual()
+        {
+            return this.manual!;
+        }
+    }
+
+    class Director
+    {
+        public void makeSUV(Builder builder) { }
+        public void makeSuportsCar(Builder builder)
+        {
+            builder.reset();
+            builder.setSeats(2);
+            builder.setEnigne(new SportsEnigne());
+            builder.setTripComputer();
+            builder.setGPS();
+        }
+
+    }
+
+
+    static void Main(string[] args)
+    {
+        Director director = new Director();
+        CarBuilder builder = new CarBuilder();
+        director.makeSuportsCar(builder);
+        Car car = builder.getResault();
+
+    }
+}
